4042fcb7e2b1120a57b27a814eaa9858cc63226e
laser <l@s3r.me>
2020-05-06 18:26:18 -0700

    code reorg and misc formatting

--- main.go
+++ main.go
@@ -18,14 +18,12 @@ import (
 	"github.com/laser/arctic-logs-webservice/types"
 )
 
-var playerLogs = make(map[string][]string)
-var playerLogsSortedKeys = make([]string, 0)
-
-var clanLogs = make(map[string][]string)
-var clanLogsSortedKeys = make([]string, 0)
-
+var alphaSortedClanNames = make([]string, 0)
+var alphaSortedLogNames = make([]string, 0)
+var alphaSortedPlayerNames = make([]string, 0)
 var logs = make(map[string]string)
-var logsSortedKeys = make([]string, 0)
+var logsByClanName = make(map[string][]string)
+var logsByPlayerName = make(map[string][]string)
 
 func init() {
 	var metadataFiles []string
@@ -57,39 +55,95 @@ func init() {
 		orig = filepath.Base(orig)
 
 		for idx := range m.PlayerNames {
-			if _, ok := playerLogs[m.PlayerNames[idx]]; !ok {
-				playerLogs[m.PlayerNames[idx]] = []string{orig}
+			if _, ok := logsByPlayerName[m.PlayerNames[idx]]; !ok {
+				logsByPlayerName[m.PlayerNames[idx]] = []string{orig}
 			} else {
-				playerLogs[m.PlayerNames[idx]] = append(playerLogs[m.PlayerNames[idx]], orig)
+				logsByPlayerName[m.PlayerNames[idx]] = append(logsByPlayerName[m.PlayerNames[idx]], orig)
 			}
 		}
 
 		for idx := range m.ClanNames {
-			if _, ok := clanLogs[m.ClanNames[idx]]; !ok {
-				clanLogs[m.ClanNames[idx]] = []string{orig}
+			if _, ok := logsByClanName[m.ClanNames[idx]]; !ok {
+				logsByClanName[m.ClanNames[idx]] = []string{orig}
 			} else {
-				clanLogs[m.ClanNames[idx]] = append(clanLogs[m.ClanNames[idx]], orig)
+				logsByClanName[m.ClanNames[idx]] = append(logsByClanName[m.ClanNames[idx]], orig)
 			}
 		}
 
 		logs[orig] = orig
 	}
 
-	for idx := range clanLogs {
-		clanLogsSortedKeys = append(clanLogsSortedKeys, idx)
+	for idx := range logsByClanName {
+		alphaSortedClanNames = append(alphaSortedClanNames, idx)
 	}
 
-	for idx := range playerLogs {
-		playerLogsSortedKeys = append(playerLogsSortedKeys, idx)
+	for idx := range logsByPlayerName {
+		alphaSortedPlayerNames = append(alphaSortedPlayerNames, idx)
 	}
 
 	for idx := range logs {
-		logsSortedKeys = append(logsSortedKeys, idx)
+		alphaSortedLogNames = append(alphaSortedLogNames, idx)
 	}
 
-	sort.Strings(clanLogsSortedKeys)
-	sort.Strings(playerLogsSortedKeys)
-	sort.Strings(logsSortedKeys)
+	sort.Strings(alphaSortedClanNames)
+	sort.Strings(alphaSortedPlayerNames)
+	sort.Strings(alphaSortedLogNames)
+}
+
+var ClanDetailPageHandler = createDetailPageHandler(logsByClanName)
+
+var PlayerDetailPageHandler = createDetailPageHandler(logsByPlayerName)
+
+var LogPageHandler = func(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+
+	f, err := os.Open(fmt.Sprintf("./logs/%s", name))
+	defer f.Close()
+	check(err)
+
+	_, err = io.Copy(w, f)
+	check(err)
+}
+
+var HomePageHandler = func(w http.ResponseWriter, r *http.Request) {
+	tpl := "home.tpl"
+
+	data := types.HomePage{
+		Players: []types.Link{},
+		Clans:   []types.Link{},
+		Logs:    []types.Link{},
+	}
+
+	for _, v := range alphaSortedClanNames {
+		data.Clans = append(data.Clans, types.Link{
+			Label: v,
+			Url:   fmt.Sprintf("/clans/%s", v),
+		})
+	}
+
+	for _, v := range alphaSortedPlayerNames {
+		data.Players = append(data.Players, types.Link{
+			Label: v,
+			Url:   fmt.Sprintf("/players/%s", v),
+		})
+	}
+
+	for _, v := range alphaSortedLogNames {
+		data.Logs = append(data.Logs, types.Link{
+			Label: v,
+			Url:   fmt.Sprintf("/logs/%s", logs[v]),
+		})
+	}
+
+	t, err := template.ParseFiles(tpl)
+	check(err)
+
+	var buf bytes.Buffer
+	err = t.Execute(&buf, data)
+	check(err)
+
+	_, err = io.Copy(w, &buf)
+	check(err)
 }
 
 func main() {
@@ -100,121 +154,47 @@ func main() {
 
 	rtr := mux.NewRouter()
 
-	rtr.HandleFunc("/clans/{name}", func(w http.ResponseWriter, r *http.Request) {
-		tpl := "detail.tpl"
-
-		name := mux.Vars(r)["name"]
-
-		logs := clanLogs[name]
-
-		data := types.DetailPage{
-			Logs: []types.Link{},
-		}
-
-		for idx := range logs {
-			data.Logs = append(data.Logs, types.Link{
-				Label: logs[idx],
-				Url:   fmt.Sprintf("/logs/%s", logs[idx]),
-			})
-		}
-
-		t, err := template.ParseFiles(tpl)
-		check(err)
-
-		var buf bytes.Buffer
-		err = t.Execute(&buf, data)
-		check(err)
-
-		_, err = io.Copy(w, &buf)
-		check(err)
-	}).Methods("GET")
-
-	rtr.HandleFunc("/players/{name}", func(w http.ResponseWriter, r *http.Request) {
-		tpl := "detail.tpl"
-
-		name := mux.Vars(r)["name"]
-
-		logs := playerLogs[name]
-
-		data := types.DetailPage{
-			Logs: []types.Link{},
-		}
-
-		for idx := range logs {
-			data.Logs = append(data.Logs, types.Link{
-				Label: logs[idx],
-				Url:   fmt.Sprintf("/logs/%s", logs[idx]),
-			})
-		}
-
-		t, err := template.ParseFiles(tpl)
-		check(err)
-
-		var buf bytes.Buffer
-		err = t.Execute(&buf, data)
-		check(err)
-
-		_, err = io.Copy(w, &buf)
-		check(err)
-	}).Methods("GET")
-
-	rtr.HandleFunc("/logs/{name}", func(w http.ResponseWriter, r *http.Request) {
-		name := mux.Vars(r)["name"]
-
-		f, err := os.Open(fmt.Sprintf("./logs/%s", name))
-		defer f.Close()
-		check(err)
-
-		_, err = io.Copy(w, f)
-		check(err)
-	}).Methods("GET")
-
-	rtr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tpl := "home.tpl"
-
-		data := types.HomePage{
-			Players: []types.Link{},
-			Clans:   []types.Link{},
-			Logs:    []types.Link{},
-		}
-
-		for _, v := range clanLogsSortedKeys {
-			data.Clans = append(data.Clans, types.Link{
-				Label: v,
-				Url:   fmt.Sprintf("/clans/%s", v),
-			})
-		}
-
-		for _, v := range playerLogsSortedKeys {
-			data.Players = append(data.Players, types.Link{
-				Label: v,
-				Url:   fmt.Sprintf("/players/%s", v),
-			})
-		}
-
-		for _, v := range logsSortedKeys {
-			data.Logs = append(data.Logs, types.Link{
-				Label: v,
-				Url:   fmt.Sprintf("/logs/%s", logs[v]),
-			})
-		}
-
-		t, err := template.ParseFiles(tpl)
-		check(err)
-
-		var buf bytes.Buffer
-		err = t.Execute(&buf, data)
-		check(err)
-
-		_, err = io.Copy(w, &buf)
-		check(err)
-	}).Methods("GET")
+	rtr.HandleFunc("/clans/{name}", ClanDetailPageHandler).Methods("GET")
+	rtr.HandleFunc("/players/{name}", PlayerDetailPageHandler).Methods("GET")
+	rtr.HandleFunc("/logs/{name}", LogPageHandler).Methods("GET")
+	rtr.HandleFunc("/", HomePageHandler).Methods("GET")
 
 	http.Handle("/", rtr)
 
 	fmt.Printf("server listening on port %s...\n", port)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func createDetailPageHandler(idx map[string][]string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl := "detail.tpl"
+
+		name := mux.Vars(r)["name"]
+
+		logs := idx[name]
+
+		data := types.DetailPage{
+			Logs: []types.Link{},
+		}
+
+		for idx := range logs {
+			data.Logs = append(data.Logs, types.Link{
+				Label: logs[idx],
+				Url:   fmt.Sprintf("/logs/%s", logs[idx]),
+			})
+		}
+
+		t, err := template.ParseFiles(tpl)
+		check(err)
+
+		var buf bytes.Buffer
+		err = t.Execute(&buf, data)
+		check(err)
+
+		_, err = io.Copy(w, &buf)
+		check(err)
+	}
 }
 
 func check(e error) {
